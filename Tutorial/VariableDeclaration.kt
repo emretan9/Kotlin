f4ef8b993dbaf943ab9f26f68ac746e74192beb9
Emre Zeki TANRIKULU <81697924+emretan9@users.noreply.github.com>
2022-09-23 19:56:36 +0300

    Add files via upload

---
+++ Tutorial/VariableDeclaration.kt
@@ -0,0 +1,28 @@
+package com.example.variable
+
+fun main(){
+    var studentName = "Emre"
+    var studentAge = 23
+    var studentHeight = 1.98 //If you want to decclare float u must type 'f' at the end
+    var stuFirstLetter = 'E'
+
+    print("Welcome\t"+studentName+"\nYour age is\t"+studentAge)
+    println("\n---------------------------------------------")
+   //Other type
+
+    var productId : Int = 1907
+    var productName : String = "Rolex watch"
+    var stock : Int = 1000
+    var productPrice :Double = 149.99 //or Float 149.99f
+    var productDealer :String = "Rolex"
+
+    print("Product ID:"+productId+"\nProduct Name:\t"+productName+"\nStock:"+stock+"\nPrice:"+productPrice+"\nDealer:"+productDealer)
+
+
+
+
+
+
+
+
+}
