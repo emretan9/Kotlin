d4c39c3b09c5f4e5f230c9af6279229a01a22226
Emre Zeki TANRIKULU <81697924+emretan9@users.noreply.github.com>
2022-09-30 19:27:35 +0300

    Add files via upload

---
+++ Tutorial/typeConversion.kt
@@ -0,0 +1,48 @@
+package com.example.tutorials
+
+fun main(){
+   var i:Int =42
+    var d:Double=42.45
+     var f:Float=92.99f
+
+    var result1:Int = d.toInt()
+    var result2:Int = f.toInt()
+    var result3:Float = i.toFloat()
+
+       println(result1)
+       println(result2)
+       println(result3)
+
+    //Convertion numeric to String
+    var str1= i.toString()
+    var str2= d.toString()
+    var str3= f.toString()
+
+    //Now they converted to String and pc see them like string
+        println("\n"+str1)
+        println(str2)
+        println(str3)
+
+    //Convertion from String to numeric
+
+    /*You need to be careful when you want to convert from String
+     to numeric because Text may include characters.So u should use
+     try cathc block in order not to get error */
+
+    var str="EZT99"
+    try {
+        val number=str.toInt()
+        println(number)
+    }catch (e:Exception){
+        print("there is an error at convertion ")
+
+    }
+
+
+
+
+
+
+
+
+}
